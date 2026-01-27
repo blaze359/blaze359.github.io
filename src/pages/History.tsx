f265f8ec3d952a606a0e39d7f5954643916ca06a
Jameson Arndt <arndtj@graco.com>
2026-01-27 15:19:20 -0500

    Fix history page to resolve build issue

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -3,7 +3,18 @@ export default function History() {
   return (
     <section>
       <h2>History</h2>
-      <p></p>
+      <ul>
+        {HistoryData.History.map((item) => (
+          <li key={item.Company}>
+            <h3>
+              {item.Company} - {item.HQLocation}
+            </h3>
+            <h5>
+              {item.StartDate} - {item.EndDate}
+            </h5>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
